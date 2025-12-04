0bfe77b4e363f4dc62bd1cc781d5d0a3359ab165
lllrrr <6922937@qq.com>
2025-12-04 14:18:50 +0800

    将子模块 libs/hbb_common 转换为普通目录

--- libs/hbb_common/src/stream.rs
+++ libs/hbb_common/src/stream.rs
@@ -1,14 +1,10 @@
 use crate::{config, tcp, websocket, ResultType};
-#[cfg(feature = "webrtc")]
-use crate::webrtc;
 use sodiumoxide::crypto::secretbox::Key;
 use std::net::SocketAddr;
 use tokio::net::TcpStream;
 
 // support Websocket and tcp.
 pub enum Stream {
-    #[cfg(feature = "webrtc")]
-    WebRTC(webrtc::WebRTCStream),
     WebSocket(websocket::WsFramedStream),
     Tcp(tcp::FramedStream),
 }
@@ -17,8 +13,6 @@ impl Stream {
     #[inline]
     pub fn set_send_timeout(&mut self, ms: u64) {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.set_send_timeout(ms),
             Stream::WebSocket(s) => s.set_send_timeout(ms),
             Stream::Tcp(s) => s.set_send_timeout(ms),
         }
@@ -27,8 +21,6 @@ impl Stream {
     #[inline]
     pub fn set_raw(&mut self) {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.set_raw(),
             Stream::WebSocket(s) => s.set_raw(),
             Stream::Tcp(s) => s.set_raw(),
         }
@@ -37,8 +29,6 @@ impl Stream {
     #[inline]
     pub async fn send_bytes(&mut self, bytes: bytes::Bytes) -> ResultType<()> {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.send_bytes(bytes).await,
             Stream::WebSocket(s) => s.send_bytes(bytes).await,
             Stream::Tcp(s) => s.send_bytes(bytes).await,
         }
@@ -47,8 +37,6 @@ impl Stream {
     #[inline]
     pub async fn send_raw(&mut self, bytes: Vec<u8>) -> ResultType<()> {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.send_raw(bytes).await,
             Stream::WebSocket(s) => s.send_raw(bytes).await,
             Stream::Tcp(s) => s.send_raw(bytes).await,
         }
@@ -57,8 +45,6 @@ impl Stream {
     #[inline]
     pub fn set_key(&mut self, key: Key) {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.set_key(key),
             Stream::WebSocket(s) => s.set_key(key),
             Stream::Tcp(s) => s.set_key(key),
         }
@@ -67,8 +53,6 @@ impl Stream {
     #[inline]
     pub fn is_secured(&self) -> bool {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.is_secured(),
             Stream::WebSocket(s) => s.is_secured(),
             Stream::Tcp(s) => s.is_secured(),
         }
@@ -80,8 +64,6 @@ impl Stream {
         timeout: u64,
     ) -> Option<Result<bytes::BytesMut, std::io::Error>> {
         match self {
-            #[cfg(feature = "webrtc")]
-            Stream::WebRTC(s) => s.next_timeout(timeout).await,
             Stream::WebSocket(s) => s.next_timeout(timeout).await,
             Stream::Tcp(s) => s.next_timeout(timeout).await,
         }
@@ -105,8 +87,6 @@ impl Stream {
     #[inline]
     pub async fn send(&mut self, msg: &impl protobuf::Message) -> ResultType<()> {
         match self {
-            #[cfg(feature = "webrtc")]
-            Self::WebRTC(s) => s.send(msg).await,
             Self::WebSocket(ws) => ws.send(msg).await,
             Self::Tcp(tcp) => tcp.send(msg).await,
         }
@@ -116,8 +96,6 @@ impl Stream {
     #[inline]
     pub async fn next(&mut self) -> Option<Result<bytes::BytesMut, std::io::Error>> {
         match self {
-            #[cfg(feature = "webrtc")]
-            Self::WebRTC(s) => s.next().await,
             Self::WebSocket(ws) => ws.next().await,
             Self::Tcp(tcp) => tcp.next().await,
         }
@@ -126,8 +104,6 @@ impl Stream {
     #[inline]
     pub fn local_addr(&self) -> SocketAddr {
         match self {
-            #[cfg(feature = "webrtc")]
-            Self::WebRTC(s) => s.local_addr(),
             Self::WebSocket(ws) => ws.local_addr(),
             Self::Tcp(tcp) => tcp.local_addr(),
         }
@@ -136,14 +112,5 @@ impl Stream {
     #[inline]
     pub fn from(stream: TcpStream, stream_addr: SocketAddr) -> Self {
         Self::Tcp(tcp::FramedStream::from(stream, stream_addr))
-    }
-
-    #[inline]
-    #[cfg(feature = "webrtc")]
-    pub fn get_webrtc_stream(&self) -> Option<webrtc::WebRTCStream> {
-        match self {
-            Self::WebRTC(s) => Some(s.clone()),
-            _ => None,
-        }
     }
 }
